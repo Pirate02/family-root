59e887474986fc218162609163ba19cd5719d3ee
Rana <dorzedumzan@gmail.com>
2026-07-26 09:14:55 +0545

    cors added

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express();
 
-app.use(cors())
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true}))
 app.use(express.json())
 
 app.use('/auth',authRouter);
